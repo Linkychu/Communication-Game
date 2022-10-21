fe0979f6b196f9beb1b2d8c05b49aa534f325349
Linkychu <77743354+Linkychu@users.noreply.github.com>
2022-10-21 15:07:20 +0100

    sdasdad
    sss

---
+++ Communication Game/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance { get; set; }
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+}
---
+++ Communication Game/Assets/Scripts/ItemClass.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Characters;
+
+
+public enum ItemType
+{
+    Regular,
+    Gold,
+    Consumable,
+    Map,
+    BossKey
+}
+
+
+
+
+[CreateAssetMenu(fileName = "items1", menuName = "Data/Items", order = 0)]
+public class ItemClass : ScriptableObject
+{
+        
+    public int rateOfAppearing;
+    public GameObject item;
+    public ItemType type;
+    public string description;
+    public Sprite image;
+    public int maxAmount;
+    public int minAmount;
+    public int HealAmount;
+    public StatBoost Boost;
+
+
+
+}
